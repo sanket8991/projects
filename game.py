699065ab75f3fd4a7322083543b7453053e8a986
Sanket Singh <75307351+sanket8991@users.noreply.github.com>
2021-01-05 12:03:07 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,56 @@
+import random  # import the random module
+
+# making a list of all the valid moves
+
+moves = ['rock', 'paper', 'scissors']
+
+
+print('welcome to a simple game of rock paper scissors\n')
+
+while True:
+    # picking up a random move for the computer from the list
+
+    c=random.choice(moves)
+
+    # taking user input for the player's move
+    p=input('make a move (rock/paper/scissors): ')
+
+    if p=='rock':
+        if c=='paper':
+            print('oops, {} gets covered by {}'.format(p,c))
+
+        elif c=='scissors':
+            print("congrats, you've won the game, {} crushes {}".format(p,c))
+
+        else:
+            print("it's a tie")
+
+    elif p == 'paper':
+        if c == 'paper':
+            print("it's a tie ")
+
+        elif c == 'scissors':
+            print("oops, {} gets cut by the {}".format(p,c))
+
+        else:
+            print("congrats, you've won the game, {} covers the {}".format(p,c))
+
+
+    elif p=='scissors':
+        if c == 'paper':
+            print("congrats, you've won the game, {} cuts the {}".format(p,c))
+
+        elif c == 'scissors':
+            print("it's a tie")
+
+        else:
+            print("oops, {} crushes the {}".format(c,p))
+
+    p=input("would you like to play the game again (y/n): ")
+
+    if p=='y':
+        continue
+    else:
+        print("thank you for playing")
+        break
+

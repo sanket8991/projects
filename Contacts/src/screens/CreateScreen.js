e0bd747ff5db8d6f82ca3c053b5760e57de181ba
Sanket Singh <75307351+sanket8991@users.noreply.github.com>
2022-06-22 13:51:32 +0530

    Update CreateScreen.js

--- Contacts/src/screens/CreateScreen.js
+++ Contacts/src/screens/CreateScreen.js
@@ -1,16 +1,10 @@
 import React, {useContext, useState} from 'react';
-
 import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
-
 import { Context } from '../context/ContactContext';
-
 import Form from '../components/Form';
 
 const CreateScreen = function({navigation}){
-
-
     const {addContact} = useContext(Context);
-
     return(
         <Form onSubmit = {(name,contact,email) => {
             addContact(name,contact,email, () => navigation.navigate('Index'))

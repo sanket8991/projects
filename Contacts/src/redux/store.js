8c29a1b563c664b782e305c9e47047979af951f8
sanket8991 <sanket.singh15@gmail.com>
2022-06-23 18:12:25 +0530

    inital commit

--- Contacts/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from './contactSlice';
-
-const store = configureStore({
-    reducer:{contacts: contactsReducer}
-});
-
-export default store;
